28a83c9a466da7184c46dedfc6a5977a3f0b018e
Alamelumangaim <114240963+Alamelumangaim@users.noreply.github.com>
2023-12-28 13:07:14 +0530

    Delete TestingUser directory

--- TestingUser/src/main/java/com/testing/testinguser/Repository/DonorformRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testing.testinguser.Repository;
-
-import com.testing.testinguser.UserData.Donorformdetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonorformRepository extends JpaRepository<Donorformdetails,Long> {
-}
